9eda748be9dd127249855e0552e0aaf4839791a3
Mainendra <1897340+mainendra@users.noreply.github.com>
2020-11-07 14:12:39 -0700

    Updates

--- _dist_/Skills.css.proxy.js
+++ _dist_/Skills.css.proxy.js
@@ -1,6 +1,6 @@
 // [snowpack] add styles to the page (skip if no document exists)
 if (typeof document !== 'undefined') {
-  const code = ".technical-skills {\n    position: relative;\n    display: flex;\n    flex-direction: column;\n    align-self: center;\n    margin: 20px;\n    width: 100%;\n    max-width: 800px;\n}\n\n.technical-skills > h3 {\n    margin: 20px 0;\n}\n\n";
+  const code = ".technical-skills {\n    position: relative;\n    flex-direction: column;\n    align-self: center;\n    margin: 20px;\n    width: 100%;\n    max-width: 800px;\n}\n\n.technical-skills > h3 {\n    margin: 20px 0;\n}\n\n";
 
   const styleEl = document.createElement("style");
   const codeEl = document.createTextNode(code);
