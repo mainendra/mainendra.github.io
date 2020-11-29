7b2c0929259139e1a0e2153d30f465b22f40da53
Mainendra <c-mainendra.patel@charter.com>
2020-11-29 14:39:40 -0700

    Prevent default on toggle click

--- src/DarkToggle.jsx
+++ src/DarkToggle.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function Sun({ onClick }) {
     return (
-        <a href="#" onClick={onClick}>
+        <a href="" onClick={onClick}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
             </svg>
@@ -12,7 +12,7 @@ function Sun({ onClick }) {
 
 function Moon({ onClick }) {
     return (
-        <a href="#" onClick={onClick}>
+        <a href="" onClick={onClick}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
             </svg>
@@ -23,7 +23,8 @@ function Moon({ onClick }) {
 export default function DarkToggle() {
     const [dark, setDark] = useState(false);
 
-    const toggleDark = (dark) => {
+    const toggleDark = (e, dark) => {
+        e.preventDefault();
         localStorage.setItem('theme', dark ? 'dark' : 'light');
         setDark(dark);
         updateThemeClass(dark);
@@ -46,7 +47,7 @@ export default function DarkToggle() {
     return (
         <div>
             {
-                dark ? <Sun onClick={() => toggleDark(!dark)}/> : <Moon onClick={() => toggleDark(!dark)}/>
+                dark ? <Sun onClick={(e) => toggleDark(e, !dark)}/> : <Moon onClick={(e) => toggleDark(e, !dark)}/>
             }
         </div>
     );
