b226ee968f4aa61dab2b487988639bb19a1b2b20
Mainendra <1897340+mainendra@users.noreply.github.com>
2021-05-31 13:02:54 -0600

    Added child transitions

--- src/DelayShow.tsx
+++ src/DelayShow.tsx
@@ -1,20 +1,31 @@
 import {Transition} from '@headlessui/react';
 import React, {Fragment} from 'react';
 
-function DelayShow({ children }: { children: React.ReactNode }) {
+export default function DelayShow({ children }: { children: React.ReactNode }) {
     return (
         <Transition
-        show={true}
-        appear={true}
-        as={Fragment}
-        enter="transition duration-[400ms]"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="duration-200 transition ease-in-out"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0">
+            show={true}
+            appear={true}
+            as={Fragment}
+            enter="transition duration-[400ms]"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="duration-200 transition ease-in-out"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0">
             {children}
         </Transition>);
 }
 
-export default DelayShow;
+export function DelayChildShow({ children, delayClass = '' }: { children: React.ReactNode, delayClass?: string }) {
+    return (
+        <Transition.Child
+            enter={`transition duration-[400ms] ${delayClass}`}
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="duration-200 transition ease-in-out"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0">
+            {children}
+        </Transition.Child>);
+}
