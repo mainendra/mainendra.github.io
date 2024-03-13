bf6fea3991d4a4a1b51712cf0f448964d46bb8b0
Mainendra <1897340+mainendra@users.noreply.github.com>
2024-03-13 08:09:28 -0400

    Used JetBrainsMonoNerdFont

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['index.html', 'src/**/*.tsx'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            custom: ['JetBrainsMonoNerdFont']
+        }
+    },
   },
   variants: {
     extend: {},
